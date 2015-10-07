fd7dbd91a24d073299b5a4a4fd0652066aea5ddf
Matías Hunicken <matihunicken@gmail.com>
2015-10-07 12:23:21 -0300

    Implemented ViterbiTagger

--- tagging/hmm.py
+++ tagging/hmm.py
@@ -4,7 +4,7 @@ SENT_START = '<s>'
 SENT_END = '</s>'
 
 
-class HMM:
+class HMM(object):
 
     def __init__(self, n, tagset, trans, out):
         """
@@ -67,7 +67,7 @@ class HMM:
             result *= self.out_prob(word, tag)
         return result
 
-    def tag_log_prob(self, y, base=2.):
+    def tag_log_prob(self, y):
         """
         Log-probability of a tagging.
 
@@ -76,21 +76,21 @@ class HMM:
         result = 0.
         prev_tags = (SENT_START,) * (self.n-1)
         for tag in y:
-            result += log(self.trans_prob(tag, prev_tags), base)
+            result += log(self.trans_prob(tag, prev_tags), 2)
             prev_tags = (prev_tags + (tag,))[1:]
-        result += log(self.trans_prob(SENT_END, prev_tags), base)
+        result += log(self.trans_prob(SENT_END, prev_tags), 2)
         return result
 
-    def log_prob(self, x, y, base=2.):
+    def log_prob(self, x, y):
         """
         Joint log-probability of a sentence and its tagging.
 
         x -- sentence.
         y -- tagging.
         """
-        result = self.tag_log_prob(y, base)
+        result = self.tag_log_prob(y)
         for word, tag in zip(x, y):
-            result += log(self.out_prob(word, tag), base)
+            result += log(self.out_prob(word, tag), 2)
         return result
 
     def tag(self, sent):
@@ -108,11 +108,33 @@ class ViterbiTagger:
         hmm -- the HMM.
         """
         self.hmm = hmm
+        self._pi = {}
 
     def tag(self, sent):
         """Returns the most probable tagging for a sentence.
 
         sent -- the sentence.
         """
-        # TODO
-        return None
+        trans = self.hmm._trans
+        self._pi = {}
+        self._pi[0] = {(SENT_START,)*(self.hmm.n-1): (log(1., 2), [])}
+        sent = tuple(sent)
+
+        for i, word in enumerate(sent):
+            self._pi[i+1] = next_pi = {}
+            for prev_tags, (prev_prob, all_tags) in self._pi[i].items():
+                for tag in trans[prev_tags]:
+                    tags = (prev_tags + (tag,))[1:]
+                    prob = prev_prob + \
+                        log(trans[prev_tags][tag], 2) + \
+                        log(self.hmm.out_prob(word, tag), 2)
+                    if tags not in next_pi or prob > next_pi[tags][0]:
+                        next_pi[tags] = (prob, all_tags + [tag])
+
+        best_prob = -float('inf')
+        best_tagging = None
+        for _, (prob, tagging) in self._pi[len(sent)].items():
+            if best_tagging is None or prob > best_prob:
+                best_tagging = tagging
+
+        return best_tagging
